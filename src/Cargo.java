bc1b8df8587007520ac49aa4f3ec0096471f119b
joelmunu <joel.munu@gmail.com>
2022-04-21 11:56:05 +0100

    Feature: Añadido el método unload a la clase cargo

--- src/Cargo.java
+++ src/Cargo.java
@@ -26,6 +26,21 @@ public class Cargo extends SpaceShip{
         }
     }
 
+    // Método unload
+    public void unload() {
+        // Comprobación
+        int contenedoresTrasDescargar = cargaActual - contenedoresACargar;
+
+        if (contenedoresTrasDescargar >= 0) {
+            cargaActual -= contenedoresACargar;
+            System.out.println("Se han descargado " + contenedoresACargar + " contenedores de la nave, la carga actual es " + cargaActual);
+
+        } else if (cargaActual == 0){
+            System.out.println("La nave ya no tiene más contenedores");
+
+        }
+    }
+
     public int getCargaMaxima() {
         return cargaMaxima;
     }
